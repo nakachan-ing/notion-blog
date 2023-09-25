b04599128d256752a29722d3738affaf0cb2293c
nakachan-ing <79064717+nakachan-ing@users.noreply.github.com>
2023-09-25 23:09:28 +0900

    syntax-highlight

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -2,6 +2,8 @@ import { getAllPosts, getSinglePost } from '@/lib/notionAPI';
 import { availableParallelism } from 'os';
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism';
 
 export const getStaticPaths = async () => {
     const allPosts = await getAllPosts();
@@ -35,7 +37,27 @@ const Post = ({post}) => {
         ))}
         
         <div className='mt-10 font-medium'>
-            <ReactMarkdown children={post.markdown}></ReactMarkdown>
+            <ReactMarkdown 
+            children={post.markdown} 
+            components={{
+                code({node, inline, className, children, ...props}) {
+                  const match = /language-(\w+)/.exec(className || '')
+                  return !inline && match ? (
+                    <SyntaxHighlighter
+                      {...props}
+                      children={String(children).replace(/\n$/, '')}
+                      style={vscDarkPlus}
+                      language={match[1]}
+                      PreTag="div"
+                    />
+                  ) : (
+                    <code {...props} className={className}>
+                      {children}
+                    </code>
+                  )
+                }
+              }}
+            ></ReactMarkdown>
         </div>
     </section>
   )
